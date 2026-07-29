1f54e60c75b8ecfec103fce6b7a1fb16642c5323
realizacja2023 <realizacja2023@gmail.com>
2026-07-29 21:00:19 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const BuyCoffeeApp());
+}
+
+class BuyCoffeeApp extends StatelessWidget {
+  const BuyCoffeeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BuyCoffee',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.orange,
+      ),
+      home: const BuyCoffeeScreen(),
+    );
+  }
+}
+
+class BuyCoffeeScreen extends StatefulWidget {
+  const BuyCoffeeScreen({super.key});
+
+  @override
+  State<BuyCoffeeScreen> createState() => _BuyCoffeeScreenState();
+}
+
+class _BuyCoffeeScreenState extends State<BuyCoffeeScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..loadRequest(Uri.parse('https://buycoffee.to'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) async {
+        if (didPop) return;
+        if (await _controller.canGoBack()) {
+          await _controller.goBack();
+        } else {
+          if (context.mounted) {
+            Navigator.of(context).pop();
+          }
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: WebViewWidget(controller: _controller),
+        ),
+      ),
+    );
+  }
+}
